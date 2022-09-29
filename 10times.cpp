49ccfc84d149252be3f3d288aba12f42000398f5
MWANGIGECHE <114657674+MWANGIGECHE@users.noreply.github.com>
2022-09-29 12:45:57 +0300

    Add files via upload

---
+++ 10times.cpp
@@ -0,0 +1,12 @@
+// program to display Hello world 10 times
+#include <iostream>
+using namespace std;
+int main ()
+{
+int i;
+for (i=1;i<=10;i++)
+{
+    cout<<"Hello world"<<i<<endl;
+}
+return 0;
+}
